288da38221a788b4851bc22fb6668f4900ce26b4
manojpannala <manojpannala@outlook.com>
2021-05-03 17:06:30 +0300

    Calling docker with build param.

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        sh 'docker build -t manojpannala/java-maven-app:2.1 .'
+        sh "docker build -t $imageName ."
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh 'docker push manojpannala/java-maven-app:2.1'
+        sh "docker push $imageName"
     }
 }
